f4b5813057dbde0e820eedb55939a67b6621ac93
Tomasz Zurkowski <doriath88@gmail.com>
2012-09-30 23:39:45 -0400

    Small enhancements and new README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_if_online_judges_configured
+
+  private
+  def check_if_online_judges_configured
+    if current_user && current_user.online_judges.empty?
+      flash[:alert] = "Please configure <a href=\"#{online_judges_url}\">online judges</a> to appear in the ranking.".html_safe
+    end
+  end
 end
