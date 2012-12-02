5248ba5a529d147c8ef1e06b722c08e37a0bd420
Tomasz Zurkowski <doriath88@gmail.com>
2012-12-02 14:14:52 -0500

    Fix tests

--- spec/lib/online_judges/english_spoj_spec.rb
+++ spec/lib/online_judges/english_spoj_spec.rb
@@ -10,7 +10,7 @@ describe OnlineJudges::EnglishSpoj do
     it 'fetches accepts using signedlist parser' do
       OnlineJudges::Spoj::SignedlistParser.
         should_receive(:new).
-        with('http://spoj.pl/status/doriath/signedlist/').
+        with('http://www.spoj.pl/status/doriath/signedlist/').
         and_return(stub(accepts: accepts))
 
       english_spoj.fetch_accepts('doriath').should == accepts
@@ -23,7 +23,7 @@ describe OnlineJudges::EnglishSpoj do
     it 'fetches accepts using signedlist parser' do
       OnlineJudges::Spoj::ProblemPage.
         should_receive(:new).
-        with('http://spoj.pl/ranks/PRIME/').
+        with('http://www.spoj.pl/ranks/PRIME/').
         and_return(stub(problem: problem))
 
       english_spoj.fetch_problem('PRIME').should == problem
