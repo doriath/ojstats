822a8db0f5a8609750afbfa996dd65118ada737d
Tomasz Zurkowski <doriath88@gmail.com>
2012-12-02 14:06:06 -0500

    Fix english spoj

--- lib/online_judges/english_spoj.rb
+++ lib/online_judges/english_spoj.rb
@@ -3,14 +3,14 @@ module OnlineJudges
     # @param [String] user_name
     # @return [Array<Accept>]
     def fetch_accepts(user_name)
-      url = "http://spoj.pl/status/#{user_name}/signedlist/"
+      url = "http://www.spoj.pl/status/#{user_name}/signedlist/"
       OnlineJudges::Spoj::SignedlistParser.new(url).accepts
     end
 
     # @param [String] problem_name
     # @return [Problem]
     def fetch_problem(problem_name)
-      url = "http://spoj.pl/ranks/#{problem_name}/"
+      url = "http://www.spoj.pl/ranks/#{problem_name}/"
       OnlineJudges::Spoj::ProblemPage.new(url).problem
     end
 
